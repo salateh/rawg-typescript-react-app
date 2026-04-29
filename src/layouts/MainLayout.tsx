733cfc1cc79d4221f6d1cf926b5c3240e3a873cc
SalatEh <234914038+salateh@users.noreply.github.com>
2026-04-29 15:28:56 +0300

    .gitattributes

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { NavBar } from "../components/GameCard/NavBar";
+import styled from "styled-components";
+
+export function MainLayout() {
+  return (
+    <div>
+      <header>
+        <NavBar />
+      </header>
+
+      <main className="content">
+        <Outlet />
+      </main>
+
+      <footer className="flex items-baseline">
+        <p>© 2026 Game Catalog | Monk Path Mode</p>
+      </footer>
+    </div>
+  );
+}
+
+const LayoutWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+
+  .content {
+    flex: 1;
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+  }
+`;
